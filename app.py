706a82e53cf1a75eaa9dcb21021a1ae7d63bd69b
Jason <bjason@gmail.com>
2015-01-23 18:52:47 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 def getVariable(prompt):
 
-  input = raw_input("Please enter something: ")
+  input = raw_input(prompt)#  "Please enter something: ")
   print "you entered", input
   return input
