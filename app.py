02df3d9af3078945d71386e62cffa30a5de8524b
Jason <bjason@gmail.com>
2015-01-24 22:51:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 
 def getVariable(prompt):
 
-  input = raw_input(prompt)#  "Please enter something: ")
+  input = raw_input(prompt) #  "Please enter something: ")
   print "you entered", input
   return input
+
+
+
+def getInputs():
+  
+  username = getVariable("Enter Username:")
+  
+  if(username.length <= 0):
+    print "Try again"
+    getInputs()
+    
+  
