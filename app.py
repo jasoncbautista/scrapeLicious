80844615deb305207311a92954de7a835da95d8c
Jason <bjason@gmail.com>
2015-01-26 22:06:22 -0800

    Fixed redundancy

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
 
 def getVariable(prompt):
+  inputX = raw_input(prompt) #  "Please enter something: ")
+  print "You entered:" +  inputX
+  
+  if(inputX.length <= 0):
+    print "Try again"
+    return getVariable(prompt)
 
-  input = raw_input(prompt) #  "Please enter something: ")
-  print "you entered", input
-  return input
-
-
+  return inputX
 
 def getInputs():
-  
   username = getVariable("Enter Username:")
-  
-  if(username.length <= 0):
-    print "Try again"
-    getInputs()
-    
   password  = getVariable("Enter Password:")
     
-  if(password.length <= 0):
-    print "Try again"
-    getInputs()
     
