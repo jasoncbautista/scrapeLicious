3efad7da745ce840f9fc38e9e6071e94640888dd
Jason <bjason@gmail.com>
2015-01-28 21:04:45 -0800

    python main

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from win32api import keybd_event
 
-
-def getVariable(prompt):
-  inputX = raw_input(prompt) #  "Please enter something: ")
-  print "You entered:" +  inputX
+class Scraply: 
+  def getVariable(prompt):
+    inputX = raw_input(prompt) #  "Please enter something: ")
+    print "You entered:" +  inputX
+    
+    if(inputX.length <= 0):
+      print "Try again"
+      return getVariable(prompt)
   
-  if(inputX.length <= 0):
-    print "Try again"
-    return getVariable(prompt)
-
-  return inputX
-
-def getInputs():
-  username = getVariable("Enter Username:")
-  password  = getVariable("Enter Password:")
+    return inputX
+  
+  def getInputs():
+    username = getVariable("Enter Username:")
+    password  = getVariable("Enter Password:")
     
     
+
+
+
+if __name__ == "__main__":
+  main()
